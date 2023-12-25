fbeb42bf00d6871eb83fcfc9af1633fa771e9745
Abdilfatah muse <abdilfatah.muse@safarifoneict.com>
2023-12-25 19:52:37 +0300

    Initial Commit

---
+++ AvdManager/AvdManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvdManagerApp.swift
+//  AvdManager
+//
+//  Created by Cabdifataax Cabdilaahi on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AvdManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvdManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AvdManager
+//
+//  Created by Cabdifataax Cabdilaahi on 25/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
